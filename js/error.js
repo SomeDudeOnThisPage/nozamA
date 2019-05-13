a6b8b7356e5f24f02cf1fb16289e24d6f6fe91a4
Robin <robin.buhlmann@gmail.com>
2019-05-14 00:15:30 +0200

    Y E S

--- js/error.js
+++ js/error.js
@@ -1,3 +1,5 @@
+let secret_on = false;
+
 window.onload = function()
 {
   let url = new URL(window.location.href);
@@ -6,4 +8,26 @@ window.onload = function()
 
   document.getElementById('error-number').innerText = id;
   document.getElementById('error-message').innerText = msg;
+
+  document.getElementById('secret').onclick = function()
+  {
+    if (!secret_on)
+    {
+      let representation = document.createElement('img');
+      representation.src = 'resources/img/db_irl.gif';
+      representation.id = 'representation';
+
+      document.getElementById('dun-dun-dun-dun-da-da-da').play();
+      document.getElementById('whats-this').appendChild(representation);
+      document.getElementById('secret').innerText = 'Okay enough stop this.';
+      secret_on = !secret_on;
+    }
+    else
+    {
+      document.getElementById('dun-dun-dun-dun-da-da-da').pause();
+      document.getElementById('secret').innerText = 'Again!';
+      document.getElementById('whats-this').removeChild(document.getElementById('representation'));
+      secret_on = !secret_on;
+    }
+  }
 };
