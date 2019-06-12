2c107e002adbc9963598bdb673872e87bc15b375
Robin <robin.buhlmann@gmail.com>
2019-06-12 16:23:53 +0200

    Made Header better n stuff

--- resources/scripts/edititem.js
+++ resources/scripts/edititem.js
@@ -1,5 +1,95 @@
 let current;
 
+document.addEventListener("ondataloaded", function(e)
+{
+  current = new URL(window.location.href).searchParams.get('item');
+
+  // Setup form callbacks (These three use the same callback as their data is shared. Separation is purely visual.)
+  $('#item-data-form')[0].setCallback(changeItemData);
+  $('#item-manufacturer-form')[0].setCallback(changeItemData);
+  $('#item-category-form')[0].setCallback(changeItemData);
+  $('#item-details-form')[0].setCallback(function()
+  {
+    let details = $('#item-details').find('dynamic-table')[0].toJSON();
+    window.QueryManager.post('CHANGE_ITEM', 'details/' + window.getCookie('sessionID') + '/' + current, details, function() {})
+  });
+
+  // Get item data
+  window.QueryManager.get('ITEM', current, function(result)
+  {
+    $('#item-data-form')[0].populate(result);
+
+    // Setup select boxes
+    window.QueryManager.get('MANUFACTURERS', null, function(data) { populateSelect('manufacturer', data, result['manufacturer']); });
+    //$('#category').append($('<option></option>').val('Test').text('Hello'));
+    window.QueryManager.get('CATEGORIES', null, function(data) { populateSelect('category', data, result['category']); });
+
+    // Setup detail table rows
+    let table = $('#item-details').find('dynamic-table')[0];
+    table.populate(result['details']);
+    $('#item-details-form')[0].disable();
+
+    // Setup Manufacturer-Add button
+    $('#add-manufacturer').keypress(function(e)
+    {
+      if (e.keyCode === 13)
+      {
+        let manufacturer = $('#add-manufacturer').val();
+        if (manufacturer === '')
+        {
+          $('#item-manufacturer-form')[0].setError('Name must not be empty!');
+          return;
+        }
+
+        // Check if the entered manufacturer already exists in our list
+        let select = $('#manufacturer');
+        $('#manufacturer').find('option').each(function ()
+        {
+          if ($(this).text().toLowerCase() === manufacturer.toLowerCase())
+          {
+            select.val(findValueByText(select, manufacturer));
+            return false;
+          }
+        });
+
+        // If not, add the manufacturer and create the option tag
+        window.QueryManager.post('ADD_MANUFACTURER', manufacturer, null, function (result)
+        {
+          let option = $('<option></option>')
+            .text(result['manufacturer_name'])
+            .val(result['manufacturer_id']);
+
+          select.append(option);
+        });
+      }
+    });
+  });
+});
+
+/**
+ * Shared callback between the toggle forms (except the item details form.).
+ */
+function changeItemData()
+{
+  let data = $('#item-data-form')[0].toJSON();
+  let manufacturer = $('#item-manufacturer-form')[0].toJSON();
+  let category = $('#item-category-form')[0].toJSON();
+  if (category['category'] === null) { category['category'] = 1; }
+
+  // There goes our IE11 support
+  let json = Object.assign(data, manufacturer, category);
+
+  console.log(json);
+
+  window.QueryManager.post('CHANGE_ITEM', 'info/' + window.getCookie('sessionID') + '/' + current, json, function() {})
+}
+
+/**
+ * Finds an option field by its' value.
+ * @param select The parent select element.
+ * @param text The text to find.
+ * @returns {*|jQuery|string|undefined}
+ */
 function findValueByText(select, text)
 {
   // Set select option to current manufacturer
@@ -10,78 +100,23 @@ function findValueByText(select, text)
   }).val();
 }
 
-document.addEventListener("ondataloaded", function(e)
+/**
+ * Convenience function to populate a select field with given data.
+ * @param element The select field.
+ * @param data The data.
+ * @param initial Initially selected option field by its' text.
+ */
+function populateSelect(element, data, initial)
 {
-  current = new URL(window.location.href).searchParams.get('item');
-
-  // Setup forms
-  $('#item-data-form')[0].setCallback(function() {});
-  $('#item-details-form')[0].setCallback(function()
+  let select = $('#' + element);
+  data.forEach(function(object)
   {
-    console.log($('#item-details').find('dynamic-table')[0].toJSON());
+    let option = $('<option></option>')
+      .text(object[element + '_name'])
+      .val(object[element + '_id']);
+
+    select.append(option);
   });
-  $('#item-manufacturer-form')[0].setCallback(function() {});
 
-  window.QueryManager.get('ITEM', current, function(result)
-  {
-    $('#item-data-form')[0].populate(result);
-
-    // Setup manufacturer list
-    window.QueryManager.get('MANUFACTURERS', null, function(data)
-    {
-      let select = $('#manufacturer');
-      Array.from(data).forEach(function(manufacturer)
-      {
-        let option = $('<option></option>')
-          .text(manufacturer['manufacturer_name'])
-          .val(manufacturer['manufacturer_id']);
-
-        select.append(option);
-      });
-
-      select.val(findValueByText(select, result['manufacturer']));
-    });
-
-    // Setup detail table rows
-    let table = $('#item-details').find('dynamic-table')[0];
-    for (let key in result['details'])
-    {
-      table.addRow([key, result['details'][key]], true);
-    }
-    // Don't forget to call disable() on the parent form again!
-    $('#item-details-form')[0].disable();
-
-    // Setup Manufacturer-Add button
-    $('#add-manufacturer-confirm').click(function()
-    {
-      let m = $('#add-manufacturer').val();
-      if (m === '')
-      {
-        $('#item-manufacturer-form')[0].setError('Name must not be empty!');
-        return;
-      }
-
-      let select = $('#manufacturer');
-
-      // Check if the entered manufacturer already exists in our list
-      $('#manufacturer').find('option').each(function()
-      {
-        if ($(this).text().toLowerCase() === m.toLowerCase())
-        {
-          select.val(findValueByText(select, m));
-          return false;
-        }
-      });
-
-      // If not, add the manufacturer and create the option tag
-      window.QueryManager.post('ADD_MANUFACTURER', m, null, function(result)
-      {
-        let option = $('<option></option>')
-          .text(result['manufacturer_name'])
-          .val(result['manufacturer_id']);
-
-        select.append(option);
-      });
-    });
-  });
-});
+  select.val(findValueByText(select, initial));
+}
